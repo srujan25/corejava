4825cc63aa1a7b729dad718f5c6f65a77c77d351
srujan25 <srujan.ketepalli@gmail.com>
2017-03-13 00:07:44 +0530

    sss

--- interview/PrintNonRepatedCharacters.java
+++ interview/PrintNonRepatedCharacters.java
@@ -12,29 +12,34 @@ public class PrintNonRepatedCharacters {
         int repeatedCount = 0;
         int nonRepeatedCount = 0;
         for (int i = 0; i < array.length; i++) {
+            boolean repeatedFound = false;
             for (int k = 0; k < repeated.length; k++) {
-                if (array[i] == repeated[k])
+                if (array[i] == repeated[k]) {
+                    repeatedFound = true;
                     break;
-            }
-            boolean present = false;
-            int foundIndex = 0;
-            for (int j = 0; j < nonRepeated.length; j++) {
-                if (array[i] == nonRepeated[j]) {
-                    present = true;
-                    foundIndex = j;
                 }
             }
-            if (!present) {
-                nonRepeated[nonRepeatedCount] = array[i];
-                nonRepeatedCount++;
-            } else {
-                repeated[repeatedCount] = array[i];
-                nonRepeated[foundIndex] = null;
-                repeatedCount++;
+            if(!repeatedFound) {
+                boolean present = false;
+                int foundIndex = 0;
+                for (int j = 0; j < nonRepeated.length; j++) {
+                    if (array[i] == nonRepeated[j]) {
+                        present = true;
+                        foundIndex = j;
+                    }
+                }
+                if (!present) {
+                    nonRepeated[nonRepeatedCount] = array[i];
+                    nonRepeatedCount++;
+                } else {
+                    repeated[repeatedCount] = array[i];
+                    nonRepeated[foundIndex] = null;
+                    repeatedCount++;
+                }
             }
-
         }
         traverse(nonRepeated);
+        //System.out.println("-------");
         //traverse(repeated);
 
     }
@@ -47,7 +52,7 @@ public class PrintNonRepatedCharacters {
     }
 
     public static void main(String[] args) {
-        Integer array[] = {1, 2, 1};
+        Integer array[] = {1,2,1,2,2};
         PrintNonRepatedCharacters pnrc = new PrintNonRepatedCharacters();
         pnrc.printNonRepatedCharacters(array);
     }
