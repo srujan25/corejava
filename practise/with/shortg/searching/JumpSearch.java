8ab1052b4265408645b4c0b67e3017e8f1d82c59
srujan25 <srujan.ketepalli@gmail.com>
2017-09-18 22:49:05 +0530

    Jump Search intial check in

---
+++ practise/with/shortg/searching/JumpSearch.java
@@ -0,0 +1,31 @@
+package practise.with.shortg.searching;
+
+public class JumpSearch {
+
+    public static String jumpSearch(int[] inputArray, int jump, int key) {
+        int jumpCount = jump;
+        for (int i = 0; i <= inputArray.length; i++) {
+            if (key == inputArray[0]) {
+                return "Key Found";
+            }
+            if (jumpCount < inputArray.length && key == inputArray[jumpCount]) {
+                return "Key Found";
+            } else if ((jumpCount >= inputArray.length && key < inputArray[inputArray.length - 1]) || (jumpCount < inputArray.length && key < inputArray[jumpCount])) {
+                int linearArray[] = new int[inputArray.length - ((jumpCount) / 2 + 1)];
+                for (int j = ((jumpCount) / 2 + 1), k = 0; j < inputArray.length; j++, k++) {
+                    linearArray[k] = inputArray[j];
+                }
+                return LinearSearch.linearSearch(key, linearArray);
+            } else {
+                jumpCount = jumpCount * 2;
+            }
+        }
+
+        return "Key didn't find";
+    }
+
+    public static void main(String[] args) {
+        int[] inputArray = {0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 114, 233, 377, 610};
+        System.out.println(jumpSearch(inputArray, 4, 0));
+    }
+}
